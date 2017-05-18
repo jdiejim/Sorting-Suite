9ac424e955b3fa08620ef21151fc42cb1fbd1f7b
Juan Diego Jimenez <jdiejim@gmail.com>
2017-05-17 23:08:49 -0600

    add index.js for exporting

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+import bubbleSort from './bubbleSort';
+import insertionSort from './bubbleSort';
+import mergeSort from './bubbleSort';
+import quickSort from './bubbleSort';
+
+const sorting = {
+  bubbleSort,
+  insertionSort,
+  mergeSort,
+  quickSort
+}
+
+export default sorting;
